6ecbb57025f71fcb7f21f55e0c674e17c13e0b1e
hadiglitch10 <hadiunknownglitch@gmail.com>
2024-04-20 16:45:21 +0200

    besm allah

---
+++ web_flask/__init__.py
@@ -0,0 +1,6 @@
+# This is an example __init__.py file for a Python package
+
+# Import modules or define variables/functions here
+# For example:
+# from .module1 import some_function
+# from .module2 import MyClass
